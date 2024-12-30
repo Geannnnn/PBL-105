7a56d701413d81e1e5bd2c10cd7c8994c8412841
Anessiuuhlulu <agnesnataliasilalahi28@gmail.com>
2024-12-30 16:46:34 +0700

    Update MK.php

--- PBL105/staff/MK.php
+++ PBL105/staff/MK.php
@@ -8,7 +8,6 @@
     <title>Stock Management</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    
     <style>
         body {
             margin: 0;
