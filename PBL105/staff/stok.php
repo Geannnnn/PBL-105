a98613721bbaf307da4f5f01967f97291b3677af
Anessiuuhlulu <agnesnataliasilalahi28@gmail.com>
2024-12-30 13:14:14 +0700

    Update stok.php

--- PBL105/staff/stok.php
+++ PBL105/staff/stok.php
@@ -15,7 +15,7 @@
     <style>
         body {
             margin: 0;
-            background-color: #F8F8FC;
+            background-color: hsla(0, 0.00%, 95.30%, 0.97);
             overflow-x: hidden;
             font-family: "Roboto Mono", monospace;
             font-optical-sizing: auto;
@@ -28,8 +28,9 @@
             left: 0;
             width: 200px;
             height: 100vh;
-            background-color: #f0e6f6;
+            background-color:  rgb(218, 199, 228);
             padding: 20px;
+            border-radius: 5px;
         }
         .content {
             margin-left: 200px; 
@@ -50,7 +51,7 @@
         }
 
         .sidebar a:hover {
-            background-color: #d1c4e9;
+            background-color: rgb(252, 246, 255);
             border-radius: 20px;
         }
 
@@ -59,16 +60,34 @@
             bottom: 20px;
             left: 20px;
         }
+
+        .stok-title {
+    font-size: 25px; 
+    font-weight: bold; 
+    text-align: center; 
+    margin-bottom: 20px; }
     </style>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-2 col-xl-2 col-md-4 col-sm-6 sidebar">
-                <a href="dashboard.php">Beranda</a>
-                <a style="background-color: #d1c4e9; color: #333; border-radius: 20px;" href="stok.php">Stok</a>
-                <a href="mk.php">Barang Masuk & Keluar</a>
-                <a href="../logout.php" style="width: 80%;" class="logout"><i class="fas fa-sign-out-alt"></i> Keluar</a>
+                <img src="http://localhost/PBL-105-main/PBL105/gambar/1.png" alt="Logo" width="140" height="80">
+                    <h3 class="stok-title" style="color: #4a00e0;">
+                        <span>STOK</span><span style="color: rgb(223, 37, 198);">STOK</span>
+                    </h3>
+                <a href="dashboard.php">
+                    <i class="fas fa-home"></i> Beranda
+                </a>
+                <a style="background-color:rgb(252, 246, 255); color: #333; border-radius: 20px;" href="#">
+                    <i class="fas fa-box"></i> Stok
+                </a>
+                <a href="MK.php">
+                    <i class="fas fa-exchange-alt"></i> Barang Masuk/Keluar
+                </a>
+                <a href="../logout.php" style="width:80%" class="logout">
+                    <i class="fas fa-sign-out-alt"></i> Keluar
+                </a>
             </div>
             
             <div class="col-lg-10 col-xl-10 col-md-8 col-sm-6 content">
