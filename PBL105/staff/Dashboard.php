df4a59af4c7399cb151f3ed4ca8dc70ca892aa01
Anessiuuhlulu <agnesnataliasilalahi28@gmail.com>
2025-01-09 13:56:41 +0700

    Update Dashboard.php

--- PBL105/staff/Dashboard.php
+++ PBL105/staff/Dashboard.php
@@ -221,6 +221,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'staff' || empty($_SESSION
                     <h3 class="stok-title" style="color: #4a00e0;">
                         <span>STOK</span><span style="color: rgb(223, 37, 198);">STOK</span>
                     </h3>
+                        <hr style="border: 1px solid rgb(159, 126, 177);; margin: 10px 0;">
 
                     <a style="background-color: rgb(252, 246, 255); color: #333; border-radius: 20px;" href="#">
                         <i class="fas fa-home"></i> Beranda
